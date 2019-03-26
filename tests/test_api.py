6a946a6ce43824647820f94d92a1ec40a9efda84
navarka.all@gmail.com <navarka.all@gmail.com>
2019-03-26 13:30:45 +0300

    Unittest parse.

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,6 @@
-import CurrencyConvert
+from CurrencyConvert import server
+
+from werkzeug.exceptions import BadRequest
 
 from unittest import TestCase, skip
 from time import sleep
@@ -6,14 +8,124 @@ from json import loads, dumps
 from requests import get, post
 import subprocess
 
-currency_rate = dict()
 
-# class ConvertTest(TestCase):
-#     def test_convert(self):
-#         CurrencyConvert.server.convert()
+class ParseTest(TestCase):
+
+    def test_json(self):
+        test_rates = { "RUR": 10, "USD": 10 }
+        res = server.parse_from_json(dumps(test_rates))
+        self.assertEqual(test_rates, res)
+
+    def test_json_type(self):
+        test_rates = {"RUR": 10, "USD": "ab"}
+        with self.assertRaises(BadRequest):
+            server.parse_from_json(dumps(test_rates))
+
+    def test_json_bad(self):
+        test_rates = {"RUR": 10, "USD": { "key": "value"}}
+        with self.assertRaises(BadRequest):
+            server.parse_from_json(dumps(test_rates))
+
+    def test_xml(self):
+        test_rates = {"RUR": 100, "USD": 1000}
+        check_data_xml = '''<?xml version="1.0"?>
+                            <data>
+                                <currency>
+                                    <name>RUR</name>
+                                    <rate>100</rate>
+                                </currency>
+                                <currency>
+                                    <name>USD</name>
+                                    <rate>1000</rate>
+                                </currency>
+                            </data>'''
+        res = server.parse_from_xml(check_data_xml)
+        self.assertEqual(test_rates, res)
+
+    def test_xml_type(self):
+        check_data_xml = '''<?xml version="1.0"?>
+                            <data>
+                                <currency>
+                                    <name>RUR</name>
+                                    <rate>RUR</rate>
+                                </currency>
+                                <currency>
+                                    <name>USD</name>
+                                    <rate>100.0</rate>
+                                </currency>
+                            </data>'''
+        with self.assertRaises(BadRequest):
+            server.parse_from_xml(check_data_xml)
+
+    def test_xml_miss_value(self):
+        check_data_xml = '''<?xml version="1.0"?>
+                            <data>
+                                <currency>
+                                    <name>RUR</name>
+                                </currency>
+                                <currency>
+                                    <name>USD</name>
+                                    <rate>100.0</rate>
+                                </currency>
+                            </data>'''
+        with self.assertRaises(BadRequest):
+            server.parse_from_xml(check_data_xml)
+
+    def test_xml_bad(self):
+        check_data_xml = '''<?xml version="1.0"?>
+                            <data>
+                                <currency>
+                                    <name>RUR</name>
+                                    <rate>100.0</rate>
+                                <currency>
+                                    <name>USD</name>
+                                    <rate>100.0</rate>
+                                </currency>
+                            </data>'''
+        with self.assertRaises(BadRequest):
+            server.parse_from_xml(check_data_xml)
+
+    def test_csv(self):
+        check_data = {
+            "RUR": 1,
+            "CSV": 2,
+            "USD": 3
+        }
+        check_data_csv = 'name,rate\nRUR,1\ncsv,2\nusd,3\n'
+        res = server.parse_from_csv(check_data_csv)
+        self.assertEqual(check_data, res)
+
+    def test_csv_missing(self):
+        check_data_csv = 'name\nRUR\ncsv\nusd\n'
+        with self.assertRaises(BadRequest):
+            server.parse_from_csv(check_data_csv)
+        check_data_csv = 'name,rate\nRUR\ncsv,2\nusd,3\n'
+        with self.assertRaises(BadRequest):
+            server.parse_from_csv(check_data_csv)
+
+    def test_csv_type(self):
+        check_data_csv = 'name,rate\nRUR,RUR\ncsv,2\nusd,3\n'
+        with self.assertRaises(BadRequest):
+            server.parse_from_csv(check_data_csv)
+
+
+
+
+class ConvertTest(TestCase):
+
+    def test_convert(self):
+        server.currency_rate = {"USD":1,"RUR":10}
+        converted = server.convert("RUR","USD",10)
+        self.assertEqual(converted, 1.0)
+
+
 
 
 class ApiTest(TestCase):
+    """
+    Not clearly unit testing but not excessive to test like this.
+    Fastest way to complex functionality test.
+    """
     def setUp(self):
         self._server = subprocess.Popen(("python","-m","CurrencyConvert"))
         sleep(1)
@@ -53,18 +165,18 @@ class ApiTest(TestCase):
             "RUR": 100,
             "USD": 100
         }
-        check_data_csv = '''<?xml version="1.0"?>
-<data>
-	<currency>
-		<name>RUR</name>
-		<rate>100</rate>
-	</currency>
-	<currency>
-		<name>USD</name>
-		<rate>100</rate>
-	</currency>
-</data>'''
-        response = post("http://127.0.0.1:5000/database?format=xml&merge=0", data=check_data_csv)
+        check_data_xml = '''<?xml version="1.0"?>
+                            <data>
+                                <currency>
+                                    <name>RUR</name>
+                                    <rate>100</rate>
+                                </currency>
+                                <currency>
+                                    <name>USD</name>
+                                    <rate>100</rate>
+                                </currency>
+                            </data>'''
+        response = post("http://127.0.0.1:5000/database?format=xml&merge=0", data=check_data_xml)
         print(response.text)
         self.assertEqual(check_data, loads(response.text))
 
